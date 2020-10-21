xsaamiir/waypoint-plugin-archive

🎉 Initial commit cloned from template

main.go
@@ -1,0 +1,24 @@
+package main
+
+import (
+	"github.com/sharkyze/waypoint-plugin-gae/builder"
+	"github.com/sharkyze/waypoint-plugin-gae/platform"
+	"github.com/sharkyze/waypoint-plugin-gae/registry"
+	"github.com/sharkyze/waypoint-plugin-gae/release"
+	sdk "github.com/hashicorp/waypoint-plugin-sdk"
+)
+
+func main() {
+	// sdk.Main allows you to register the components which should
+	// be included in your plugin
+	// Main sets up all the go-plugin requirements
+
+	sdk.Main(sdk.WithComponents(
+		// Comment out any components which are not
+		// required for your plugin
+		&builder.Builder{},
+		&registry.Registry{},
+		&platform.Platform{},
+		&release.ReleaseManager{},
+	))
+}

release/release.go
@@ -1,0 +1,76 @@
+package release
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/sharkyze/waypoint-plugin-gae/registry"
+	"github.com/hashicorp/waypoint-plugin-sdk/terminal"
+)
+
+type ReleaseConfig struct {
+	Active bool "hcl:directory,optional"
+}
+
+type ReleaseManager struct {
+	config ReleaseConfig
+}
+
+// Implement Configurable
+func (rm *ReleaseManager) Config() interface{} {
+	return &rm.config
+}
+
+// Implement ConfigurableNotify
+func (rm *ReleaseManager) ConfigSet(config interface{}) error {
+	_, ok := config.(*ReleaseConfig)
+	if !ok {
+		// The Waypoint SDK should ensure this never gets hit
+		return fmt.Errorf("Expected *ReleaseConfig as parameter")
+	}
+
+	// validate the config
+
+	return nil
+}
+
+// Implement Builder
+func (rm *ReleaseManager) ReleaseFunc() interface{} {
+	// return a function which will be called by Waypoint
+	return rm.release
+}
+
+// A BuildFunc does not have a strict signature, you can define the parameters
+// you need based on the Available parameters that the Waypoint SDK provides.
+// Waypoint will automatically inject parameters as specified
+// in the signature at run time.
+//
+// Available input parameters:
+// - context.Context
+// - *component.Source
+// - *component.JobInfo
+// - *component.DeploymentConfig
+// - *datadir.Project
+// - *datadir.App
+// - *datadir.Component
+// - hclog.Logger
+// - terminal.UI
+// - *component.LabelSet
+
+// In addition to default input parameters the platform.Deployment from the Deploy step
+// can also be injected.
+//
+// The output parameters for ReleaseFunc must be a Struct which can
+// be serialzied to Protocol Buffers binary format and an error.
+// This Output Value will be made available for other functions
+// as an input parameter.
+//
+// If an error is returned, Waypoint stops the execution flow and
+// returns an error to the user.
+func (rm *ReleaseManager) release(ctx context.Context, ui terminal.UI, artifact *registry.Artifact) (*Release, error) {
+	u := ui.Status()
+	defer u.Close()
+	u.Update("Release application")
+
+	return &Release{}, nil
+}

xsaamiir/waypoint-plugin-cloudfunctions

main.go
@@ -1,0 +1,24 @@
+package main
+
+import (
+	sdk "github.com/hashicorp/waypoint-plugin-sdk"
+	"github.com/sharkyze/waypoint-plugin-cloudfunctions/builder"
+	"github.com/sharkyze/waypoint-plugin-cloudfunctions/platform"
+	"github.com/sharkyze/waypoint-plugin-cloudfunctions/registry"
+	"github.com/sharkyze/waypoint-plugin-cloudfunctions/release"
+)
+
+func main() {
+	// sdk.Main allows you to register the components which should
+	// be included in your plugin
+	// Main sets up all the go-plugin requirements
+
+	sdk.Main(sdk.WithComponents(
+		// Comment out any components which are not
+		// required for your plugin
+		&builder.Builder{},
+		&registry.Registry{},
+		&platform.Platform{},
+		&release.ReleaseManager{},
+	))
+}

release/release.go
@@ -1,0 +1,76 @@
+package release
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/waypoint-plugin-sdk/terminal"
+	"github.com/sharkyze/waypoint-plugin-cloudfunctions/registry"
+)
+
+type ReleaseConfig struct {
+	Active bool "hcl:directory,optional"
+}
+
+type ReleaseManager struct {
+	config ReleaseConfig
+}
+
+// Implement Configurable
+func (rm *ReleaseManager) Config() interface{} {
+	return &rm.config
+}
+
+// Implement ConfigurableNotify
+func (rm *ReleaseManager) ConfigSet(config interface{}) error {
+	_, ok := config.(*ReleaseConfig)
+	if !ok {
+		// The Waypoint SDK should ensure this never gets hit
+		return fmt.Errorf("Expected *ReleaseConfig as parameter")
+	}
+
+	// validate the config
+
+	return nil
+}
+
+// Implement Builder
+func (rm *ReleaseManager) ReleaseFunc() interface{} {
+	// return a function which will be called by Waypoint
+	return rm.release
+}
+
+// A BuildFunc does not have a strict signature, you can define the parameters
+// you need based on the Available parameters that the Waypoint SDK provides.
+// Waypoint will automatically inject parameters as specified
+// in the signature at run time.
+//
+// Available input parameters:
+// - context.Context
+// - *component.Source
+// - *component.JobInfo
+// - *component.DeploymentConfig
+// - *datadir.Project
+// - *datadir.App
+// - *datadir.Component
+// - hclog.Logger
+// - terminal.UI
+// - *component.LabelSet
+
+// In addition to default input parameters the platform.Deployment from the Deploy step
+// can also be injected.
+//
+// The output parameters for ReleaseFunc must be a Struct which can
+// be serialzied to Protocol Buffers binary format and an error.
+// This Output Value will be made available for other functions
+// as an input parameter.
+//
+// If an error is returned, Waypoint stops the execution flow and
+// returns an error to the user.
+func (rm *ReleaseManager) release(ctx context.Context, ui terminal.UI, artifact *registry.Artifact) (*Release, error) {
+	u := ui.Status()
+	defer u.Close()
+	u.Update("Release application")
+
+	return &Release{}, nil
+}